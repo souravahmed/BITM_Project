32350090e3bbefadacb1805aa40ffa51385cfd08
souravahmed <souravahmeddiu@gmail.com>
2015-09-27 20:58:32 -0700

    First commit

---
+++ UCRMSApp/UCRMSApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UCRMSApp.Startup))]
+namespace UCRMSApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
